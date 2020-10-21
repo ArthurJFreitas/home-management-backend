fc0fc9a98842ff29559690aec2dbeb782fd314c5
nicolas <nicolasprodmac@gmail.com>
2020-10-21 01:03:49 -0300

    feature: added update home controller function

--- src/routes/homes.routes.ts
+++ src/routes/homes.routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { CreateHome, DeleteHome, ListHomes, ListOneHome } from '../controllers/home'
+import { CreateHome, DeleteHome, ListHomes, ListOneHome, UpdateHome } from '../controllers/home'
 
 const homeRouter = Router();
 
@@ -7,6 +7,7 @@ homeRouter.post("/new", CreateHome)
 homeRouter.delete("/delete/:id", DeleteHome)
 homeRouter.get("/", ListHomes)
 homeRouter.get("/:id", ListOneHome)
+homeRouter.put("/:id", UpdateHome)
 
 
 export default homeRouter;
