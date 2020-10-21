9c6bbd8e92a8004ac10f8a8243aaae884f6dde55
nicolas <nicolasprodmac@gmail.com>
2020-10-21 00:29:53 -0300

    feature: added list one home controller function

--- src/routes/homes.routes.ts
+++ src/routes/homes.routes.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
-import { CreateHome, DeleteHome, ListHomes } from '../controllers/home'
+import { CreateHome, DeleteHome, ListHomes, ListOneHome } from '../controllers/home'
 
 const homeRouter = Router();
 
 homeRouter.post("/new", CreateHome)
 homeRouter.delete("/delete", DeleteHome)
 homeRouter.get("/", ListHomes)
+homeRouter.get("/:id", ListOneHome)
 
 
 export default homeRouter;
